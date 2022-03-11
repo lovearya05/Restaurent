902f3fa4aac99fa8e67bd8f31760e957bf012436
lovearya <lovepreetarya1405@gmail.com>
2022-03-11 09:06:32 +0530

    Restaurent proj

---
+++ dom/script.js
@@ -0,0 +1,3 @@
+var newelement = document.createElement("h2");
+newelement.textContent = "Hello , i am h2 tag";
+document.querySelector("body").appendChild(newelement);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Restaurent from "./component/Restaurent/Restaurent.js";
+
+
+const App = () => {
+  return (
+    <>
+      <div>
+      <Restaurent />
+      </div>
+      
+    </>
+  );
+};
+export default App
